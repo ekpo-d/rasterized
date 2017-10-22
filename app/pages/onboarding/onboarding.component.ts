9428a2fc25eaf6d57e2ad6c0c55c72f82d8bcd27
Ekpo David <ekpodave@gmail.com>
2017-10-22 21:05:00 +0100

    Base commit

---
+++ app/pages/onboarding/onboarding.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "rs-onboarding",
+    templateUrl: "pages/onboarding/onboarding.html",
+    styleUrls: ["pages/onboarding/onboarding-common.css"],
+})
+export class OnboardingComponent implements OnInit {
+
+    ngOnInit(): void {
+    }
+}
