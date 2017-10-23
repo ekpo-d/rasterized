f7929aa986304d27e89a913224c1acbe6b9daaf0
Ekpo David <ekpodave@gmail.com>
2017-10-23 08:57:40 +0100

    Added scss

--- app/pages/onboarding/onboarding.component.ts
+++ app/pages/onboarding/onboarding.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { Page } from 'tns-core-modules/ui/page';
 
 @Component({
     selector: "rs-onboarding",
     templateUrl: "pages/onboarding/onboarding.html",
     styleUrls: ["pages/onboarding/onboarding-common.css"],
 })
-export class OnboardingComponent implements OnInit {
-
-    ngOnInit(): void {
+export class OnboardingComponent{
+    constructor(private page: Page){
+        page.actionBarHidden = true;
     }
 }
